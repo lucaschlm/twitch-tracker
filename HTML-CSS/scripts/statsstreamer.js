4ab155fe403c348a86bc14b71204a34ac7742cd4
Lucas Chalmandrier <lucas.chalmandrier@etu.uca.fr>
2022-05-12 15:52:58 +0200

    Mise en place dépendances JS

--- HTML-CSS/scripts/statsstreamer.js
+++ HTML-CSS/scripts/statsstreamer.js
@@ -11,10 +11,46 @@ input.addEventListener("keypress", (e) => {
         api = 
         {
             method: "GET",
-            url: "http://localhost:3000/".concat(input.value.toLowerCase())
+            url: "http://localhost:3002/".concat(input.value.toLowerCase())
         };
-        update();
+        // update();
     }
   });
 
-  
+const labels = ["January", "February", "March", "April", "May", "June"];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "My First dataset",
+      backgroundColor: "rgb(255, 99, 132)",
+      borderColor: "rgb(255, 99, 132)",
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }
+  ]
+};
+
+const config = {
+  type: "line",
+  data: data,
+  options: {}
+};
+
+const myChart = new Chart(document.getElementById("myChart"), config);
+
+// const update = () => {
+//     axios.get("");
+//     axios
+//       .request(api)
+//       .then(function (response) {
+
+//       })
+//       .catch(function (error) {
+//         // handle error
+//         console.log(error);
+//       })
+//       .then(function () {
+//         // always executed
+//       });
+// };
