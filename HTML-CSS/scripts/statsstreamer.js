983682c9ec20827057e3ad03cc1bec61231cd5e9
Lucas Chalmandrier <lucas.chalmandrier43@gmail.com>
2022-05-23 20:54:02 +0200

    Ajout graphiques

--- HTML-CSS/scripts/statsstreamer.js
+++ HTML-CSS/scripts/statsstreamer.js
@@ -2,31 +2,26 @@ const axios = require("axios").default;
 
 const input = document.getElementById("searchStreamer");
 
-
-let api = "";
+let name = ""
 
 input.addEventListener("keypress", (e) => {
     if (e.key === 'Enter')
     {
-        api = 
-        {
-            method: "GET",
-            url: "http://localhost:3002/".concat(input.value.toLowerCase())
-        };
-        // update();
+        name = input.value.toLowerCase();
+        update();
     }
   });
 
-const labels = ["January", "February", "March", "April", "May", "June"];
+const labels = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"];
 
 const data = {
   labels: labels,
   datasets: [
     {
-      label: "My First dataset",
-      backgroundColor: "rgb(255, 99, 132)",
-      borderColor: "rgb(255, 99, 132)",
-      data: [0, 10, 5, 2, 20, 30, 45]
+      label: "",
+      backgroundColor: "#EB5D1B",
+      borderColor: "#EB5D1B",
+      data: [null, null, null, null, null, null, null, null, null, null, null, null]
     }
   ]
 };
@@ -37,20 +32,47 @@ const config = {
   options: {}
 };
 
-const myChart = new Chart(document.getElementById("chartStreamedTime"), config);
 
-// const update = () => {
-//     axios.get("");
-//     axios
-//       .request(api)
-//       .then(function (response) {
+const update = () => {
+    axios.get("http://localhost:3000/".concat(name)) //On récupère l'api
 
-//       })
-//       .catch(function (error) {
-//         // handle error
-//         console.log(error);
-//       })
-//       .then(function () {
-//         // always executed
-//       });
-// };
+      .then(function (response) {
+    //////////////////////////////////////////
+    //             TEMPS STREAME            //
+    //////////////////////////////////////////        
+        console.log(response.data);
+        data.datasets[0].label = "Temps streamé"; //On change le label
+        response.data.forEach(element => { //On parcourt toutes les valeurs (tous les mois du streamer)
+          const date = new Date(element.date); //On transforme la date en format Date pour pouvoir la traiter facilement
+          data.datasets[0].data[date.getMonth() - 1] = element.minutes_streamed / 60; //On change la valeur pour le graphique
+        });
+        const streamedTimeChart = new Chart(document.getElementById("chartStreamedTime"), config);
+    //////////////////////////////////////////
+    //            VIEWERS MOYENS            //
+    //////////////////////////////////////////
+        console.log(response.data);
+        data.datasets[0].label = "Viewers moyens"; //On change le label
+        response.data.forEach(element => { //On parcourt toutes les valeurs (tous les mois du streamer)
+          const date = new Date(element.date); //On transforme la date en format Date pour pouvoir la traiter facilement
+          data.datasets[0].data[date.getMonth() - 1] = element.avg_viewers; //On change la valeur pour le graphique
+        });
+        const avgViewersChart = new Chart(document.getElementById("chartAvgViewers"), config);
+    //////////////////////////////////////////
+    //             VIEWERS MAX              //
+    //////////////////////////////////////////
+        console.log(response.data);
+        data.datasets[0].label = "Viewers max"; //On change le label
+        response.data.forEach(element => { //On parcourt toutes les valeurs (tous les mois du streamer)
+          const date = new Date(element.date); //On transforme la date en format Date pour pouvoir la traiter facilement
+          data.datasets[0].data[date.getMonth() - 1] = element.max_viewers; //On change la valeur pour le graphique
+        });
+        const maxViewersChart = new Chart(document.getElementById("chartMaxViewers"), config);
+    })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+      .then(function () {
+        // always executed
+      });
+};
